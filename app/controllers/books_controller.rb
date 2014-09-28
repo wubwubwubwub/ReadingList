977827c70a69effd63eaefd9a9fea9c4fd25e503
wubwubwubwub <nolan.camp@gmail.com>
2014-09-28 15:46:49 -0400

    add javascript to slide new book form up after create, reverse book order

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-     @books = Book.includes(:genres).search(params[:keyword]).filter(params[:filter])
+     @books = Book.includes(:genres).search(params[:keyword]).filter(params[:filter]).reverse
      @genres = Genre.all
   end
 
@@ -31,6 +31,7 @@ class BooksController < ApplicationController
       if @book.save
         format.html { redirect_to @book, notice: 'Book was successfully created.' }
         format.json { render :show, status: :created, location: @book }
+        format.js
       else
         format.html { render :new }
         format.json { render json: @book.errors, status: :unprocessable_entity }
